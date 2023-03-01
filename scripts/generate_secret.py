86ed7a00e69dc8e176b86d308fd6e8845b54f3ca
Jaime Botia Lanzas <47253032+JaimeBotiaLanzas@users.noreply.github.com>
2023-03-01 16:11:11 +0100

    Update generate_secret.py

--- scripts/generate_secret.py
+++ scripts/generate_secret.py
@@ -4,3 +4,4 @@ import rstr
 print(f"AWS_KEYID: {rstr.xeger(r'AKIA[A-Z2-7]{16}')}")
 print(f"AWS_SECRET: {rstr.xeger(r'[0-9A-Za-z/+=]{40}')}")
 print(f"DATADOG_TOKEN: {rstr.xeger(r'[a-f0-9]{32}|[a-f0-9]{40}')}")
+print("Key=7EBZgyesSkVx+16m48J6jtScD5ib4wGooMBoQAtLYP2jueODOLFEuI1PnhBcJhBWy/dCRO/pBpZe+AStmT110g==")
